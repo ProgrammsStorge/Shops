531e2b48248d67e3457ccab8b86aae29502ee5b7
ProgrammsStorge <blohroma@gmail.com>
2025-11-27 19:50:05 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-# Shops
+# Pizzeria
+Программа для заказа пицце делающая упор на простату использования
 
-Это система магазинов на python.
+![GitHub top language](https://img.shields.io/github/languages/top/ProgrammsStorge/Shops)
+![GitHub Repo stars](https://img.shields.io/github/stars/ProgrammsStorge/Shops)
 
-Блох Роман ИИ-71
+![Logotype](./img/logo.png)
+
+## Установка
+У вас должны быть установлен python <b>(желательно 3.13)</b>
+
+1. Клонирование репозитория 
+
+```git clone https://github.com/ProgrammsStorge/Shops.git```
+
+2. Переход в директорию Shops
+
+```cd Shops```
+
+3. Установка зависимостей
+
+```pip install -r requirements.txt```
+
+4. Запуск скрипта
+
+```python main.py```
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
+import math
 import os
+import random
+import re
 import sys
 import time
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import colorama
 import keyboard
 from rich.console import Console
@@ -38,11 +41,64 @@ def gradient_text(text,color,end="\n",step=10):
 
 def draw_image(file_path="pizza.jpg",size=10):
     img = Image.open(file_path)
-    for colon in range(size):
-        for row in range(size):
-            color=img.getpixel((img.size[0]//size*row, img.size[0]//size*colon))
-            for _ in range(2):Console(soft_wrap=True).print(RichText("█"), style=f"rgb({color[0]},{color[1]},{color[2]})",end="")
-        print()
+    try:
+        for colon in range(size):
+            for row in range(size):
+                color=img.getpixel((img.size[0]//size*row, img.size[0]//size*colon))
+                for _ in range(2):Console(soft_wrap=True).print(RichText("█"), style=f"rgb({color[0]},{color[1]},{color[2]})",end="")
+            print()
+    except:pass
+
+def captcha():
+    try:
+        def make_pairs(ind_words):
+            for i in range(len(ind_words) - 1):
+                yield (ind_words[i], ind_words[i + 1])
+        with open("captcha_generate_text.txt","r",encoding="utf-8") as f:
+            data=f.read()
+        ind_words = data.split()
+        pair = make_pairs(ind_words)
+        word_dict = {}
+        for word_1, word_2 in pair:
+            if word_1 in word_dict.keys():
+                word_dict[word_1].append(word_2)
+            else:
+                word_dict[word_1] = [word_2]
+
+        first_word = random.choice(ind_words)
+        while first_word.islower():
+            chain = [first_word]
+            n_words = 2
+            first_word = random.choice(ind_words)
+
+            for i in range(n_words):
+                chain.append(random.choice(word_dict[chain[-1]]))
+        code = ' '.join(chain).upper()#''.join([random.choice('qwertyuiopasdfghjklzxcvbnm1234567890'.upper()) for i in range(10)])
+        width, height = 600//2, 50
+        img = Image.new('RGB', (width, height), (0, 0, 0))
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("arial.ttf", 15)
+        x = 0
+        y = 12
+        for i,let in enumerate(code):
+            if x == 0:
+                x = 5
+            else:
+                x = x + width / len(code)
+            y = math.sin(i)*10+10
+            draw.text((x, y), let, font=font,
+                      fill=(random.randint(30, 200), random.randint(30, 200), random.randint(30, 200), 128))
+        for i in range(10):
+            draw.line([(random.randint(0, width), random.randint(0, height)),
+                       (random.randint(0, width), random.randint(0, height))],
+                      random.randint(0, 200), 2, 128)
+
+        img.save("captcha.jpg", "PNG")
+        return code.upper()
+    except:captcha()
+
+
+
 def read_file(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         return f.read()
@@ -59,6 +115,7 @@ class Item():
     def add_compound(self,*add):
         for ing in add:
             self.ingredients.append(ing)
+            self.price+=10
 
     def remove_compound(self,*rem):
         for ing in rem:
@@ -148,29 +205,37 @@ class Shop():
             print()
         #print(colorama.Fore.RED+f"{(len(self.description)//2 - len(self.name)//2 -3) * '-'} {self.name} {(len(self.description)//2 - len(self.name)//2 -3) * '-'}")
         #print(f"{colorama.Fore.GREEN+self.description}")
+        for i, item in enumerate(self.shop_list.items):
+            if not (item.age == (self.user.age >= 18) or self.user.age >= 18):
+                self.shop_list.items.remove(item)
         for i,item in enumerate(self.shop_list.items):
-            if i==select:print()
-            print(colorama.Fore.YELLOW + "●", item.name,
-                  (colorama.Back.RED + colorama.Fore.BLACK + "(+18)" + colorama.Back.RESET) * int(item.age),(colorama.Back.YELLOW + colorama.Fore.BLACK + f"СКИДКА {item.sale}%" + colorama.Back.RESET) * int(
-                        item.sale != 0),
-                        colorama.Fore.RED + f"{self.in_garbage(item.id)} в корзине" * int(self.in_garbage(item.id) != 0),end=" "
-                  )
-            if self.liked(item.id): gradient_text("Лайкнуто",color=[255,255,94])
-            else:print()
-            if i == select:
-                print(colorama.Fore.YELLOW + "├──> ", item.description,
-                      (colorama.Back.YELLOW + colorama.Fore.BLACK + f"СКИДКА {item.sale}%" + colorama.Back.RESET) * int(
-                          item.sale != 0))
-                if item.sale != 0:print(colorama.Fore.YELLOW + "├──> ",
-                      (colorama.Back.YELLOW + colorama.Fore.BLACK + item.sale_description + colorama.Back.RESET) * int(
-                          item.sale != 0))
-                print(colorama.Fore.YELLOW + "├──> ","Ингредиенты:",
-                      (colorama.Back.RESET + colorama.Fore.RED + ", ".join(item.ingredients) + colorama.Back.RESET))
-                print(colorama.Fore.YELLOW + "└──> ", "Цена:",
-                      (colorama.Back.RESET + colorama.Fore.RESET+(colorama.Back.YELLOW + colorama.Fore.BLACK)*int(item.sale !=0) + str(item.price-(item.price//100*item.sale))+"р"
+            if item.age==(self.user.age >= 18) or self.user.age >= 18:
+                if i==select:print()
+                print(colorama.Fore.YELLOW + "●", item.name,
+                      (colorama.Back.RED + colorama.Fore.BLACK + "(+18)" + colorama.Back.RESET) * int(item.age),(colorama.Back.YELLOW + colorama.Fore.BLACK + f"СКИДКА {item.sale}%" + colorama.Back.RESET) * int(
+                            item.sale != 0),
+                            colorama.Fore.RED + f"{self.in_garbage(item.id)} в корзине" * int(self.in_garbage(item.id) != 0),end=" "
+                      )
+                if self.liked(item.id): gradient_text("Лайкнуто",color=[255,255,94])
+                else:print()
+                if i == select:
+                    print(colorama.Fore.YELLOW + "├──> ", item.description,
+                          (colorama.Back.YELLOW + colorama.Fore.BLACK + f"СКИДКА {item.sale}%" + colorama.Back.RESET) * int(
+                              item.sale != 0))
+                    if item.sale != 0:print(colorama.Fore.YELLOW + "├──> ",
+                          (colorama.Back.YELLOW + colorama.Fore.BLACK + item.sale_description + colorama.Back.RESET) * int(
+                              item.sale != 0))
+                    print(colorama.Fore.YELLOW + "├──> ","Ингредиенты:",
+                          (colorama.Back.RESET + colorama.Fore.RED + ", ".join(item.ingredients) + colorama.Back.RESET))
+                    print(colorama.Fore.YELLOW + "└──> ", "Цена:",
+                          (colorama.Back.RESET + colorama.Fore.RESET+(colorama.Back.YELLOW + colorama.Fore.BLACK)*int(item.sale !=0) + str(item.price-(item.price//100*item.sale))+"р"
 
-                       + colorama.Back.RESET))
-                if i == select: print()
+                           + colorama.Back.RESET))
+                    if i == select: print()
+            else:
+                if i == select:
+                    print("skip")
+                    select+=1
 
     def in_garbage(self,id):
         i=0
@@ -202,9 +267,14 @@ class Shop():
             #                                                style=f"rgb({color[0]},{color[1]},{color[2]})", end="")
             if len(text_in_image) > colon: print(text_in_image[colon], end="")
             print()
+        print()
+        print(colorama.Fore.GREEN + "-"*20)
+        print("ЧЕК:")
         full_price=0
         age_need_allow=False
         have_any_item=False
+        have_ingrs=get_file("ingredients","ingredients.json")
+        have_all_ingrs=True
         for i, item in enumerate(self.shop_list.items):
             if self.in_garbage(item.id)!=0:
                 have_any_item=True
@@ -218,15 +288,25 @@ class Shop():
                       )
                 full_price = full_price + self.in_garbage(item.id)*(item.price-(item.price//100*item.sale))
                 print(colorama.Fore.RESET+str( self.in_garbage(item.id)*(item.price-(item.price//100*item.sale)))+"р")
+                for ingr in item.ingredients:
+                    if ingr in have_ingrs and have_ingrs[ingr]!=0:
+                        have_ingrs[ingr]-=1
+                    else:
+                        have_all_ingrs=False
         print()
         print(colorama.Fore.GREEN + "Итог:", full_price)
+        save_file("ingredients",have_ingrs,"ingredients.json")
         if age_need_allow and self.user.age<18:
             print(colorama.Fore.GREEN + "В вашем заказе есть товары которые нельзя продавать с эти возрастом!")
+            return False
+        if not have_all_ingrs:
+            print(colorama.Fore.GREEN + "У нас нет ингредиентов.")
             return False
         if not have_any_item:
             print(colorama.Fore.GREEN + "В вашем заказе нет товаров!")
             return False
-        print(colorama.Fore.GREEN+"Итог:",full_price)
+        print(colorama.Fore.GREEN + "-"*20)
+        print(colorama.Fore.GREEN + "Оплата:")
         print(colorama.Fore.GREEN+"< Наличка | Безнал >")
         while True:
             if keyboard.is_pressed('left'):
@@ -247,7 +327,7 @@ class Shop():
                     except Exception as e:
                         print("Ошибка при оплате:", e)
                 break
-            if keyboard.is_pressed('right'):
+            elif keyboard.is_pressed('right'):
                 time.sleep(0.5)
                 print("Денег у вас есть:", self.user.wallet.money_card)
                 try:
@@ -256,7 +336,31 @@ class Shop():
                 except Exception as e:
                     print("Ошибка при оплате:", e)
                 break
+    def edit_item(self,item):
+        os.system("cls")
+        text_in_image = [f"{colorama.Fore.RED + self.name + colorama.Fore.RESET}"] + f"{self.description}".split("\n")
+        # img = Image.open("pizza.jpg")
 
+        # size=len(self.description)//8
+        size = len(text_in_image)
+        for colon in range(size):
+            for row in range(size):
+                pass
+            # color = img.getpixel((img.size[0] // size * row, img.size[0] // size * colon))
+            # for _ in range(2): Console(soft_wrap=True).print(RichText("█"),
+            #                                                style=f"rgb({color[0]},{color[1]},{color[2]})", end="")
+            if len(text_in_image) > colon: print(text_in_image[colon], end="")
+            print()
+
+        while True:
+            _name=input("Имя: ")
+            itegrs=input(colorama.Fore.GREEN +f"Какие ингредиенты вы хотите добавить в {_name}? (Через запитую): ")
+            if itegrs.replace(" ","")!="" and _name.replace(" ","")!="":break
+        #for itegr in itegrs.split(","):
+        self.shop_list.items.append(Item(_name,"Товар созданный пользователем",False,itegrs.split(","),len(itegrs.split(","))*300,0,"" ,-1))
+             #.add_compound(itegr.replace(" ","").lower()))
+        gradient_text("Готово!",[255,255,255])
+        time.sleep(2)
     def menu_loop(self):
         select=0
         self.draw_menu(select)
@@ -268,28 +372,35 @@ class Shop():
                 self.draw_menu(select)
                 time.sleep(0.5)
 
-            if keyboard.is_pressed('down'):
+            elif keyboard.is_pressed('down'):
                 select+=1
                 if select >= len(self.shop_list.items): select = len(self.shop_list.items)-1
                 self.draw_menu(select)
                 time.sleep(0.5)
 
-            if keyboard.is_pressed('enter'):
+            elif keyboard.is_pressed('enter'):
                 garbage=get_file("garbage",self.save_file)
                 if garbage==None:garbage=[]
                 garbage.append(self.shop_list.items[select].id)
                 save_file("garbage",garbage, self.save_file)
                 self.draw_menu(select)
                 time.sleep(0.5)
-
-            if keyboard.is_pressed('backspace'):
+            elif keyboard.is_pressed('ctrl'):
+                self.shop_list.update_list(self.decoded_json["items"])
+                self.draw_menu(select)
+                time.sleep(0.5)
+            elif keyboard.is_pressed('shift'):
+                self.edit_item(self.shop_list.items[select])
+                self.draw_menu(select)
+                time.sleep(0.5)
+            elif keyboard.is_pressed('backspace'):
                 garbage=get_file("garbage",self.save_file)
                 if garbage==None:garbage=[]
                 if self.shop_list.items[select].id in garbage:garbage.remove(self.shop_list.items[select].id)
                 save_file("garbage",garbage, self.save_file)
                 self.draw_menu(select)
                 time.sleep(0.5)
-            if keyboard.is_pressed('right'):
+            elif keyboard.is_pressed('right'):
                 likes=get_file("likes",self.save_file)
                 if likes==None:likes=[]
                 if self.liked(self.shop_list.items[select].id):likes.remove(self.shop_list.items[select].id)
@@ -297,7 +408,7 @@ class Shop():
                 save_file("likes",likes, self.save_file)
                 self.draw_menu(select)
                 time.sleep(0.5)
-            if keyboard.is_pressed('left'):
+            elif keyboard.is_pressed('left'):
                 self.draw_garbage()
                 time.sleep(0.5)
                 while True:
@@ -310,6 +421,14 @@ class Shop():
                 self.draw_menu()
                 time.sleep(0.5)
 
+def save_on_change_wallet(metod):
+    def saving(self, *args, **kwargs):
+        metod(self,*args,**kwargs)
+        if self.save_load:
+            self.save()
+    return saving
+
+
 class Wallet():
     def __init__(self,money_card,cash,save_file="save.json",save_load=True):
         self.money_card=money_card
@@ -317,17 +436,18 @@ class Wallet():
         self.save_file = save_file
         self.save_load=save_load
         if save_load:self.load()
+
+    @save_on_change_wallet
     def edit_cash(self,count):
         if (self.cash + count)<0:
             raise ValueError("There is no money to pay")
         else:self.cash+=count
-        if self.save_load:self.save()
 
+    @save_on_change_wallet
     def edit_money_card(self,count):
         if (self.money_card + count)<0:
             raise ValueError("There is no money to pay")
         else:self.money_card+=count
-        if self.save_load:self.save()
 
     def save(self):
         save_file("money_card",self.money_card,self.save_file)
@@ -345,8 +465,24 @@ class User():
         if get_file("name",self.save_file)==None:
             while True:
                 try:
-                    self.name=input("Ваше имя: ")
-                    self.age=int(input("Ваш возраст: "))
+
+                    while True:
+                        print("Пройдите captcha для продолжения регистрации.\nОткройте файл captcha.jpg и введите фразу из него.")
+                        if not (captcha() == input().upper()):
+                            print("Неверно!")
+                            continue
+
+                        self.name = input("Ваше ФИО: ")
+                        self.age=int(input("Ваш возраст: "))
+
+                        #print("Возраст превышает лимит")
+                        self.tel="".join(re.findall(r"\d",input("Ваш телефон: ")))
+                        self.mail=re.findall(r"\w+@\w+\.\w+",input("Ваш почта: "))
+                        if self.name[0].isupper() and len(self.mail)==1 and( self.age > 0 and self.age <= 200) and self.tel.replace(" ","")!="" and self.name.replace(" ", "") != "":
+
+                            break
+
+                        #print("Почта набрана неправильно.")
                     break
                 except:print()
             if save_load: self.save()
@@ -355,10 +491,14 @@ class User():
     def save(self):
         save_file("name",self.name,self.save_file)
         save_file("age", self.age, self.save_file)
+        save_file("tel", self.tel, self.save_file)
+        save_file("mail", self.mail, self.save_file)
 
     def load(self):
         if get_file("name",self.save_file)!=None:self.name=get_file("name",self.save_file)
         if get_file("age",self.save_file)!=None:self.age=get_file("age", self.save_file)
+        if get_file("tel",self.save_file)!=None:self.tel=get_file("tel",self.save_file)
+        if get_file("mail",self.save_file)!=None:self.mail=get_file("mail", self.save_file)
 
 
 if __name__=="__main__":
